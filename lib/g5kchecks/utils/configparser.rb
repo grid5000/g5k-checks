7c504f910279951012e485fad59d5d77687d3756
Émile Morel <emile@bleuchtang.fr>
2013-01-27 09:56:04 +0100

    add a simple parser class

---
+++ lib/g5kchecks/utils/configparser.rb
@@ -0,0 +1,26 @@
+require 'yaml'
+
+module G5kChecks
+      class ConfigParser
+
+        def initialize(configpath)
+          # The config path
+          @path = configpath
+          # Hash to be return after the file parsing
+          @hash = {}
+        end
+
+        def parse
+          begin
+            config = YAML.load_file(@path)
+          rescue ArgumentError
+            raise ArgumentError.new("Invalid YAML file '#{@path}'")
+          rescue Errno::ENOENT
+            raise ArgumentError.new("File not found '#{@path}'")
+          end
+          @hash[:checks_for_init_dir] = config["checks_for_init_dir"]
+          @hash[:testlist] = config["checks_for_oar"].split(',') if config["checks_for_oar"]
+          return @hash
+        end
+    end
+end
