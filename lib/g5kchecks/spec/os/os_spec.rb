635f0205be04a5317c507e68a1aba8c9700f60df
Émile Morel <emile@bleuchtang.fr>
2013-01-29 10:55:34 +0100

    [os] values inversion

--- lib/g5kchecks/spec/os/os_spec.rb
+++ lib/g5kchecks/spec/os/os_spec.rb
@@ -8,7 +8,7 @@ describe "OS" do
     it "should be the correct name" do
       name_api = ""
       name_api = @os_api['name'] if @os_api
-      name_lshw = @os_lshw[:platform_version]
+      name_lshw = @os_lshw[:platform]
       name_lshw.should eql(name_api), "#{name_lshw}, #{name_api}, operating_system, name"
     end
 
@@ -22,7 +22,7 @@ describe "OS" do
     it "should be the correct release" do
       release_api = ""
       release_api = @os_api['release'] if @os_api
-      release_lshw = @os_lshw[:platform]
+      release_lshw = @os_lshw[:platform_version]
       release_lshw.should eql(release_api), "#{release_lshw}, #{release_api}, operating_system, release"
     end
 
