c763bdebfc37c223d9b316b1b5e7c2926126395c
Émile Morel <emile@bleuchtang.fr>
2013-02-08 11:59:08 +0100

    [scripts] add merge with an other yaml file (reference-repository for instance)

--- scripts/getconfig.rb
+++ scripts/getconfig.rb
@@ -3,7 +3,11 @@ $: << File.join(File.dirname(__FILE__))
 require 'campaign'
 require 'merge_yaml'
 
-# $1 site, $2 cluster $3 nb nodes (can put ALL and BEST)
+# $1 site, $2 cluster $3 nb nodes (can put ALL and BEST), $4 file to merge
+if ARGV.size != 4
+  puts 'pas les bons arguments'
+  exit 1
+end
 ok = G5kchecksCampaign.new(ARGV[0],ARGV[1],ARGV[2])
 
-MergeYaml.new(ARGV[0],ARGV[1]).merge! if ok
+MergeYaml.new(ARGV[0],ARGV[1], ARGV[3]).merge! if ok
--- scripts/merge_yaml.rb
+++ scripts/merge_yaml.rb
@@ -1,24 +1,83 @@
 #!/usr/bin/env ruby
 $: << File.join(File.dirname(__FILE__))
 
+# Hack to enable nested Hashes merging
+# Thanks Luc !
+class Hash
+  def merge!(hash)
+    return unless hash.is_a?(Hash)
+    hash.each_pair do |k,v|
+      if self[k]
+        if v.is_a?(Hash)
+          self[k].merge!(v)
+        elsif v.is_a?(Array)
+          # Keep array's order
+          v.each_index do |i|
+            self[k][i] = v[i] unless v[i].nil?
+          end
+        else
+          self[k] = v
+        end
+      else
+        self[k] = v
+      end
+    end
+  end
+
+  def merge(hash)
+    ret = self.dup
+    return ret unless hash.is_a?(Hash)
+    hash.each_pair do |k,v|
+      if ret[k]
+        if v.is_a?(Hash)
+          ret[k] = ret[k].merge(v)
+        elsif v.is_a?(Array)
+          # Keep array's order
+          v.each_index do |i|
+            ret[k][i] = v[i] unless v[i].nil?
+          end
+        else
+          ret[k] = v
+        end
+      else
+        ret[k] = v
+      end
+    end
+    ret
+  end
+end
 
 class MergeYaml
 
-  def initialize(site, cluster)
+  def initialize(site, cluster, file_merge)
     @site = site
     @cluster = cluster
+    @file_merge = file_merge
   end
 
   def merge!
-    cluster = {}
+    checks_yaml = {}
     Dir.foreach(File.join(ARGV[0],ARGV[1])) {|x|
       next if x == '.' or x == '..'
-      cluster[x] = YAML.load_file(File.join(ARGV[0],ARGV[1],x))
+      node_name = x.split(".")[0]
+      checks_yaml["#{node_name}"] = YAML.load_file(File.join(ARGV[0],ARGV[1],x))["#{x}"]
+   }
+
+    file_check = File.join(ARGV[0],ARGV[1], "cluster_check.yaml")
+    file_merge = File.join(ARGV[0],ARGV[1], "cluster_merge_with_admin.yaml")
+    File.delete(file_check) if File.exist?(file_check)
+    File.delete(file_merge) if File.exist?(file_merge)
+    File.open(file_check, 'w') { |f|
+      f.puts checks_yaml.to_yaml
     }
 
-    File.open(File.join(ARGV[0],ARGV[1], "cluster.yaml"), 'w') { |f|
-      f.puts cluster.to_yaml
+    admin_yaml = YAML.load_file(@file_merge)
+    checks_yaml.merge!(admin_yaml)
+
+    File.open(file_merge, 'w') { |f|
+      f.puts checks_yaml.to_yaml
     }
+
   end
 
 end
