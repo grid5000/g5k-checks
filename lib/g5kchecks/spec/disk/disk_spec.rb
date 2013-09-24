1b853e594d021ae846b065097e14f0173c774b8f
Emile Morel <emile@bleuchtang.fr>
2013-09-24 19:52:59 +0200

    [disk] refactoring disk_spec.rb

--- lib/g5kchecks/spec/disk/disk_spec.rb
+++ lib/g5kchecks/spec/disk/disk_spec.rb
@@ -1,43 +1,45 @@
 describe "Disk" do
 
   before(:all) do
-    @api = RSpec.configuration.node.api_description["storage_devices"]
+    tmpapi = RSpec.configuration.node.api_description["storage_devices"]
+    if tmpapi != nil
+      @api = {}
+      tmpapi.each{ |d|
+        @api[d["device"]] = d
+      }
+    end
   end
 
-  RSpec.configuration.node.ohai_description.block_device.select { |key,value| key =~ /[sh]d.*/ and value["model"] != "vmDisk" }.each_with_index do |disk,i|
+  RSpec.configuration.node.ohai_description.block_device.select { |key,value| key =~ /[sh]d.*/ and value["model"] != "vmDisk" }.each { |k,v|
 
     it "should have the correct name" do
-      name_ohai = disk[i]
-      name_api = ""
-      name_api = @api[i]["device"] if @api
-      name_ohai.should eql(name_api), "#{name_ohai}, #{name_api}, block_devices, #{disk[0]}, device"
+      name_api = @api[k] if @api
+      name_api.should_not eql(nil), "#{k}, not_exist, block_devices, #{k}, device"
     end
 
     it "should have the correct size" do
-      size_ohai = disk[1]["size"].to_i*512
+      size_ohai = v["size"].to_i*512
       size_api = 0
-      size_api = @api[i]["size"].to_i if @api
-      size_ohai.should eql(size_api), "#{size_ohai}, #{size_api}, block_devices, #{disk[0]}, size"
+      size_api = @api[k]["size"].to_i if (@api and @api[k] and @api[k]["size"])
+      size_ohai.should eql(size_api), "#{size_ohai}, #{size_api}, block_devices, #{k}, size"
     end
 
     it "should have the correct model" do
-      model_ohai = disk[1]["model"]
-      model_ohai.force_encoding("UTF-8")
+      model_ohai = v['model']
       model_api = ""
-      model_api = @api[i]['model'] if @api
-      model_ohai.should eql(model_api), "#{model_ohai}, #{model_api}, block_devices, #{disk[0]}, model"
+      model_api = @api[k]['model'] if (@api and @api[k] and @api[k]['model'])
+      model_ohai.should eql(model_api), "#{model_ohai}, #{model_api}, block_devices, #{k}, model"
     end
 
     it "should have the correct revision" do
-      version_ohai = disk[1]["rev"]
-      version_ohai.force_encoding("UTF-8")
+      version_ohai = v["rev"]
       version_api = ""
-      version_api = @api[i]['rev'] if @api
+      version_api = @api[k]['rev'] if (@api and @api[k] and @api[k]['rev'])
       version_api = version_api.to_f if version_api.to_f != 0.0
       version_ohai = version_ohai.to_f if version_ohai.to_f != 0.0
-      version_ohai.should eql(version_api), "#{version_ohai}, #{version_api}, block_devices, #{disk[0]}, rev"
+      version_ohai.should eql(version_api), "#{version_ohai}, #{version_api}, block_devices, #{k}, rev"
     end
 
-  end
+  }
 
 end
