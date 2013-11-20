ca0ea0e86a54c0930051dcd0552adeed2728c734
Émile Morel <emile.morel@inria.fr>
2013-11-20 14:19:08 +0100

    factorization search line function

--- lib/g5kchecks/ohai/disk.rb
+++ lib/g5kchecks/ohai/disk.rb
@@ -1,22 +1,27 @@
 provides "filesystem/improve"
 require_plugin("filesystem")
 
+require 'g5kchecks/utils/utils'
+
 # complete les infos des sytèmes de fichier avec le fstab
 file_fstab = File.open("/etc/fstab")
 fstab = file_fstab.read
 file_fstab.close
 fstab.each_line do |line|
-  next if line =~ /^#/
-  if line =~ /([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*/
-    filesys = Regexp.last_match(1)
-    filesystem[filesys] = Mash.new unless filesystem.has_key?(filesys)
-    filesystem[filesys]["fs_type"] = Regexp.last_match(3)
-    filesystem[filesys]["dump"] = Regexp.last_match(5)
-    filesystem[filesys]["pass"] = Regexp.last_match(6)
-    filesystem[filesys]["options"] = Regexp.last_match(4).split(",")
-    filesystem[filesys]["mount_point"] = Regexp.last_match(2)
-    filesystem[filesys]["file_system"] = filesys
-  end
+  tmp_fs = Utils.parse_line_fstab(line)
+  next if tmp_fs == nil
+  filesystem.merge!(tmp_fs)
+#  next if line =~ /^#/
+#  if line =~ /([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*/
+#    filesys = Regexp.last_match(1)
+#    filesystem[filesys] = Mash.new unless filesystem.has_key?(filesys)
+#    filesystem[filesys]["fs_type"] = Regexp.last_match(3)
+#    filesystem[filesys]["dump"] = Regexp.last_match(5)
+#    filesystem[filesys]["pass"] = Regexp.last_match(6)
+#    filesystem[filesys]["options"] = Regexp.last_match(4).split(",")
+#    filesystem[filesys]["mount_point"] = Regexp.last_match(2)
+#    filesystem[filesys]["file_system"] = filesys
+#  end
 end
 
 if File.exist?("/proc/cmdline")
@@ -31,37 +36,40 @@ if File.exist?("/proc/cmdline")
     popen4("parted #{root_device} print") do |pid, stdin, stdout, stderr|
       stdin.close
       stdout.each do |line|
-        next if line =~ /^$/
-        if line =~ /^\s([\d]*)[\s]*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)$/
-          num = Regexp.last_match(1)
-          layout[num] = Hash.new
-          layout[num][:start] = Regexp.last_match(2)
-          layout[num][:end] = Regexp.last_match(3)
-          layout[num][:size] = Regexp.last_match(4)
-          layout[num][:type] = Regexp.last_match(5)
-          six = Regexp.last_match(6)
-          sev = Regexp.last_match(7)
-          if Regexp.last_match(5) =~ /extended/
-            layout[num][:fs] = ""
-            layout[num][:flags] = six
-          else
-            layout[num][:fs] = six
-            layout[num][:flags] = sev
-          end
-          if num == "2" or num == "3" or num == "5"
-            popen4("tune2fs -l #{root_device}#{num}") do |pid, stdin, stdout, stderr|
-              stdin.close
-              stdout.each do |line2|
-                if line2 =~ /^Filesystem state:/
-                  layout[num][:state] = line.chomp.split(": ").last.strip
-                end
+	num, tmp_layout = Utils.parse_line_layout(line)
+	next if tmp_layout == nil
+	layout.merge!(tmp_layout)
+        if num == "2" or num == "3" or num == "5"
+          popen4("tune2fs -l #{root_device}#{num}") do |pid, stdin, stdout, stderr|
+            stdin.close
+            stdout.each do |line2|
+              if line2 =~ /^Filesystem state:/
+                layout[num][:state] = line.chomp.split(": ").last.strip
               end
             end
           end
-          layout[num][:state] = "clean" if !layout[num].has_key?("state")
         end
+        layout[num][:state] = "clean" if !layout[num].has_key?("state")
       end
     end
-    filesystem["layout"]= layout
+    filesystem["layout"] = layout
   end
 end
+#        next if line =~ /^$/
+#        if line =~ /^\s([\d]*)[\s]*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)$/
+#          num = Regexp.last_match(1)
+#          layout[num] = Hash.new
+#          layout[num][:start] = Regexp.last_match(2)
+#          layout[num][:end] = Regexp.last_match(3)
+#          layout[num][:size] = Regexp.last_match(4)
+#          layout[num][:type] = Regexp.last_match(5)
+#          six = Regexp.last_match(6)
+#          sev = Regexp.last_match(7)
+#          if Regexp.last_match(5) =~ /extended/
+#            layout[num][:fs] = ""
+#            layout[num][:flags] = six
+#          else
+#            layout[num][:fs] = six
+#            layout[num][:flags] = sev
+#          end
+
--- lib/g5kchecks/utils/utils.rb
+++ lib/g5kchecks/utils/utils.rb
@@ -1,4 +1,5 @@
 # provide some useful functions
+require 'popen4'
 
 class String
   def force_encoding(enc)
@@ -54,30 +55,38 @@ module Utils
       cmdline = file.read
       file.close
       cmdline.each_line do |line|
-        next if line =~ /^$/
-        if line =~ /^\s([\d]*)[\s]*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)$/
-          num = Regexp.last_match(1)
-          layout[num] = Hash.new
-          layout[num][:start] = Regexp.last_match(2)
-          layout[num][:end] = Regexp.last_match(3)
-          layout[num][:size] = Regexp.last_match(4)
-          layout[num][:type] = Regexp.last_match(5)
-          six = Regexp.last_match(6)
-          sev = Regexp.last_match(7)
-          if Regexp.last_match(5) =~ /extended/
-            layout[num][:fs] = ""
-            layout[num][:flags] = six
-          else
-            layout[num][:fs] = six
-            layout[num][:flags] = sev
-          end
-        end
+        num, parsed_line = Utils.parse_line_layout(line)
+	layout.merge!(parsed_line) if parsed_line != nil
       end
-    else # first exectution time (after new deployment)
+    else # first execution (after new deployment)
       %x{mkdir -p #{File.join(File.dirname(__FILE__))}/../data/}
       %x{parted /dev/sda print > #{File.join(File.dirname(__FILE__), '/../data/layout')} 2>/dev/null}
     end
     layout
+  end
+
+  def Utils.parse_line_layout(line)
+    return if line =~ /^$/
+    layout = Hash.new
+    if line =~ /^\s([\d]*)[\s]*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)$/
+      num = Regexp.last_match(1)
+      layout[num] = Hash.new
+      layout[num][:start] = Regexp.last_match(2)
+      layout[num][:end] = Regexp.last_match(3)
+      layout[num][:size] = Regexp.last_match(4)
+      layout[num][:type] = Regexp.last_match(5)
+      six = Regexp.last_match(6)
+      sev = Regexp.last_match(7)
+      if Regexp.last_match(5) =~ /extended/
+        layout[num][:fs] = ""
+        layout[num][:flags] = six
+      else
+        layout[num][:fs] = six
+        layout[num][:flags] = sev
+      end
+      return num, layout
+    end
+    return ""
   end
 
   def Utils.fstab
@@ -87,21 +96,65 @@ module Utils
       fstab = file_fstab.read
       file_fstab.close
       fstab.each_line do |line|
-        next if line =~ /^#/
-        if line =~ /([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*/
-          filesys = Regexp.last_match(1)
-          filesystem[filesys] = Mash.new unless filesystem.has_key?(filesys)
-          filesystem[filesys]["fs_type"] = Regexp.last_match(3)
-          filesystem[filesys]["dump"] = Regexp.last_match(5)
-          filesystem[filesys]["pass"] = Regexp.last_match(6)
-          filesystem[filesys]["options"] = Regexp.last_match(4).split(",")
-          filesystem[filesys]["mount_point"] = Regexp.last_match(2)
-          filesystem[filesys]["file_system"] = filesys
-        end
+	parsed_line = Utils.parse_line_fstab(line)
+        filesystem.merge!(parsed_line) if parsed_line != nil
       end
-    else # first exectution time (after new deployment)
+    else # first execution (after new deployment)
       %x{mkdir -p #{File.join(File.dirname(__FILE__))}/../data/}
       %x{cp /etc/fstab #{File.join(File.dirname(__FILE__), '/../data/')}}
+    end
+    filesystem
+  end
+
+  def Utils.parse_line_fstab(line)
+    return if line =~ /^#/
+    filesystem = Hash.new
+    if line =~ /([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*([^\s]*)\s*/
+      filesys = Regexp.last_match(1)
+      filesystem[filesys] = Mash.new unless filesystem.has_key?(filesys)
+      filesystem[filesys]["fs_type"] = Regexp.last_match(3)
+      filesystem[filesys]["dump"] = Regexp.last_match(5)
+      filesystem[filesys]["pass"] = Regexp.last_match(6)
+      filesystem[filesys]["options"] = Regexp.last_match(4).split(",")
+      filesystem[filesys]["mount_point"] = Regexp.last_match(2)
+      filesystem[filesys]["file_system"] = filesys
+    end
+    filesystem
+  end
+
+  def Utils.mount_grep(grep)
+    mount = Hash.new
+    POpen4::popen4("mount | grep '#{grep} '") do |stdout, stderr, stdin, pid|
+      stdin.close
+      stdout.each do |line|
+	parsed_line = Utils.parse_line_mount(line)
+	mount.merge!(parsed_line) if parsed_line != nil
+      end
+   end
+    mount
+  end
+
+  def Utils.mount
+    mount = Hash.new
+    POpen4::popen4("mount") do |stdout, stderr, stdin, pid|
+      stdin.close
+      stdout.each do |line|
+	parsed_line = Utils.parse_line_mount(line)
+	mount.merge!(parsed_line) if parsed_line != nil
+      end
+   end
+    mount
+  end
+
+  def Utils.parse_line_mount(line)
+    return if line =~ /^#/
+    filesystem = Hash.new
+    if line =~ /([^\s]*)\s*on\s*([^\s]*)\stype*\s*([^\s]*)\s*([^\s]*)\s*/
+      mp = Regexp.last_match(1)
+      filesystem[mp] = Mash.new unless filesystem.has_key?(mp)
+      filesystem[mp]["on"] = Regexp.last_match(2)
+      filesystem[mp]["type"] = Regexp.last_match(3)
+      filesystem[mp]["options"] = Regexp.last_match(4).gsub(/[()]/,"").split(",")
     end
     filesystem
   end
