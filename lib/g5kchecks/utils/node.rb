740425a09cd8bfc51d327cb88d44d222a28985f7
Émile Morel <emile.morel@inria.fr>
2013-12-13 14:23:57 +0100

    option refactoring

--- lib/g5kchecks/utils/node.rb
+++ lib/g5kchecks/utils/node.rb
@@ -11,40 +11,51 @@ module Grid5000
   class Node
     attr_reader :hostname
     attr_reader :node_uid, :cluster_uid, :site_uid, :grid_uid
-    attr_reader :api, :node_uri, :conf
+    attr_reader :api, :node_path, :conf
 
     def initialize(conf)
       @conf = conf
       @hostname = Socket.gethostname
       @node_uid, @site_uid, @grid_uid, @ltd = hostname.split(".")
       @cluster_uid = @node_uid.split("-")[0]
-      if conf["branch"] == nil
-        @branch=""
-      else
-        @branch="?branch="+conf["branch"]
-      end
-      @node_uri = [
-        conf["urlapi"],
-        "sites", site_uid,
-        "clusters", cluster_uid,
-        "nodes", node_uid
-      ].join("/")
       @ohai_description = nil
+      @api_description = nil
     end
 
     def api_description
+      return @api_description if @api_description != nil
       if @conf["mode"] == "api"
-        @api_description ||= JSON.parse "{}"
-      else
-        begin
-         @api_description = JSON.parse RestClient.get(@node_uri+@branch, :accept => :json)
+        @api_description = JSON.parse "{}"
+      elsif @conf["retrieve_from"] == "rest"
+        if conf["branch"] == nil
+          @branch=""
+        else
+          @branch="?branch="+conf["branch"]
+        end
+        @node_path = [
+          conf["retrieve_url"],
+          "sites", site_uid,
+          "clusters", cluster_uid,
+          "nodes", node_uid
+        ].join("/")
+	begin
+          @api_description = JSON.parse RestClient.get(@node_path+@branch, :accept => :json)
         rescue RestClient::ResourceNotFound
           if @conf["fallback_branch"] != nil
-            @api_description = JSON.parse RestClient.get(@node_uri+"?branch="+@conf["fallback_branch"], :accept => :json)
-          end
+	    begin
+              @api_description = JSON.parse RestClient.get(@node_path+"?branch="+@conf["fallback_branch"], :accept => :json)
+            rescue RestClient::ResourceNotFound
+	      raise "Node not find with url #{@node_path+@branch} and #{@node_path+"?branch="+@conf["fallback_branch"]}"
+	    end
+          else
+	    raise "Node not find with url #{@node_path+@branch}"
+	  end
         end
-        return @api_description
+      elsif @conf["retrieve_from"] == "file"
+	@node_path = File.join(@conf["retrieve_url"], @hostname, "json") 
+	@api_description = JSON.parse(File.read(@node_path))	
       end
+      return @api_description
     end
 
     def get_wanted_mountpoint
