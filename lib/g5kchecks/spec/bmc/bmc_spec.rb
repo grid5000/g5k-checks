38c9e4e00b1b1cd730e1fa250b942177c18dd129
Émile Morel <emile.morel@inria.fr>
2013-12-16 11:54:14 +0100

    [bmc] strongest tests

--- lib/g5kchecks/spec/bmc/bmc_spec.rb
+++ lib/g5kchecks/spec/bmc/bmc_spec.rb
@@ -9,22 +9,25 @@ describe "BMC" do
 
     it "should have the correct IPv4" do
       ip_api = ""
+      ip_ohai = ""
       ip_api = @api['ip'] if @api
-      ip_ohai = @ohai['ip']
+      ip_ohai = @ohai['ip'] if @ohai 
       ip_ohai.should eql(ip_api), "#{ip_ohai}, #{ip_api}, network_interfaces, bmc, ip"
     end
 
     it "should have the correct Mac Address" do
       mac_api = ""
+      mac_ohai = ""
       mac_api = @api['mac'] if @api
-      mac_ohai = @ohai['mac']
+      mac_ohai = @ohai['mac'] if @ohai
       mac_ohai.should eql(mac_api), "#{mac_ohai}, #{mac_api}, network_interfaces, bmc, mac"
     end
 
     it "should be a management card" do
-      mgt_api = nil
+      mgt_api = ""
+      mgt_ohai = ""
       mgt_api = @api['management'] if @api
-      mgt_ohai = @ohai[:management]
+      mgt_ohai = @ohai[:management] if @ohai
       mgt_ohai.should eql(mgt_api), "#{mgt_ohai}, #{mgt_api}, network_interfaces, bmc, management"
     end
 end
