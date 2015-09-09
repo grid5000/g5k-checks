a7e7ef1843a773dd8ec0664466a1c43bf2630e4a
Simon Delamare <simon.delamare@ens-lyon.fr>
2015-09-09 14:22:38 +0200

    Use hwloc utils instead of ohai to check mem

--- lib/g5kchecks/spec/mem/mem_spec.rb
+++ lib/g5kchecks/spec/mem/mem_spec.rb
@@ -2,15 +2,13 @@ describe "Memory" do
 
   before do
     @api = RSpec.configuration.node.api_description['main_memory']
-    @system = RSpec.configuration.node.ohai_description
   end
 
   it "should have the correct size" do
     size_api = 0
     size_api = @api['ram_size'].to_i if @api
-    size_ohai = @system[:memory][:total].to_i
-    size_ohai = size_ohai*1024
-    (size_ohai/100000000).should eq((size_api/100000000)), "#{size_ohai}, #{size_api}, main_memory, ram_size"
+    size_sys = Utils.hwloc_parse_mem
+    (size_sys/(1024**2)).should eq(size_api/(1024**2)), "#{size_sys}, #{size_api}, main_memory, ram_size"
   end
 
 end
