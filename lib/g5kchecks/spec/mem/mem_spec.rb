0822df4291d7533c8567a1c315a16a5a7709253b
Simon Delamare <simon.delamare@ens-lyon.fr>
2015-12-02 14:53:58 +0100

    Be more tolerant for memory checking

--- lib/g5kchecks/spec/mem/mem_spec.rb
+++ lib/g5kchecks/spec/mem/mem_spec.rb
@@ -8,7 +8,8 @@ describe "Memory" do
     size_api = 0
     size_api = @api['ram_size'].to_i if @api
     size_sys = Utils.hwloc_parse_mem
-    (size_sys/(1024**2)).should eq(size_api/(1024**2)), "#{size_sys}, #{size_api}, main_memory, ram_size"
+    err = ((size_sys-size_api)/size_api.to_f).abs
+    err.should be < 0.15, "#{size_sys}, #{size_api}, main_memory, ram_size"
   end
 
 end
