acf5040b5c0a17c389e7c7e4880a43107d914384
Jérémie Gaidamour <jeremie.gaidamour@inria.fr>
2016-06-21 16:48:41 +0200

    [cpu] Added information about ht/cstates/pstates/turboboost

--- lib/g5kchecks/ohai/cpu.rb
+++ lib/g5kchecks/ohai/cpu.rb
@@ -2,6 +2,26 @@
 provides "cpu/improve"
 require_plugin("cpu")
 
+require 'open3'
+
+# Read a file. Return an array if the file constains multiple lines. Return nil if the file does not exist.
+def fileread(filename)
+  output = File.read(filename).split("\n")
+  output.size == 1 ? output[0] : output
+end
+
+# Execute an external program
+def execute(cmd)
+  stdout, stderr, status = Open3.capture3(cmd)
+  
+  raise "#{cmd}: #{status.exitstatus}" unless status.success?
+
+  stdout = stdout.split("\n")
+  stdout = (stdout.size == 1 ? stdout[0] : stdout)
+
+  return stdout
+end
+
 # We assume that every cores have the same values
 # Intel ou AMD ?
 if cpu[:'0'][:model_name] =~ /AMD/
@@ -57,3 +77,89 @@ popen4("lscpu") do |pid, stdin, stdout, stderr|
     end
   end
 end
+
+#cpu[:extra] = 'test'
+
+# 
+cpu[:cpu_speed] = (execute('x86info').last.split(" ").last.to_f * 1000 * 1000).to_i rescue 'unknown'
+
+# Parsing 'lscpu -p' output to retrieve :nb_procs, :nb_cores and :nb_threads
+# 'lscpu -p' output format :
+## CPU,Core,Socket,Node,,L1d,L1i,L2,L3
+#0,0,0,0,,0,0,0,0
+#1,0,0,0,,0,0,0,0
+#2,1,0,0,,1,1,1,0
+#3,1,0,0,,1,1,1,0
+
+lscpu_p = execute('lscpu -p').grep(/^[^#]/) # skip header lines
+lscpu_p.map!{ |line| line.split(',') }   # split each line
+lscpu_p = lscpu_p.transpose              # transpose the data
+lscpu_p_count = lscpu_p.map { |line| line.uniq.count } # count
+
+cpu[:nb_procs]   = lscpu_p_count[2]
+cpu[:nb_cores]   = lscpu_p_count[1]
+cpu[:nb_threads] = lscpu_p_count[0]
+
+# :ht_capable
+cpu_flags = fileread('/proc/cpuinfo').grep(/flags\t\t: /)[0] rescue 'unknown'
+cpu[:ht_capable] = ! / ht /.match(cpu_flags).nil?
+
+# :ht_enabled
+cpu[:ht_enabled] = ! (cpu[:nb_threads] == cpu[:nb_cores])
+
+# pstate
+cpu[:pstate_driver]        = fileread('/sys/devices/system/cpu/cpu0/cpufreq/scaling_driver') rescue 'none'
+cpu[:pstate_governor]      = fileread('/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor') rescue 'none'
+
+if cpu[:pstate_driver] != 'none'
+  cpu[:pstate_max_cpu_speed] = fileread('/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq').to_i rescue nil
+  cpu[:pstate_min_cpu_speed] = fileread('/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq').to_i rescue nil
+end
+
+# :turboboost_enabled
+if cpu[:pstate_driver] == 'intel_pstate'
+  cpu[:turboboost_enabled] = (fileread('/sys/devices/system/cpu/intel_pstate/no_turbo') == "0") rescue "unknown"
+elsif cpu[:pstate_driver] == 'acpi-cpufreq'
+  cpu[:turboboost_enabled] = (fileread('/sys/devices/system/cpu/cpufreq/boost') == "1") rescue "unknown"
+else
+  cpu[:turboboost_enabled] = "unknown"
+end
+
+# cstate
+cpu[:cstate_driver]   = fileread('/sys/devices/system/cpu/cpuidle/current_driver') rescue 'none'
+cpu[:cstate_governor] = fileread('/sys/devices/system/cpu/cpuidle/current_governor_ro') rescue 'none'
+
+if cpu[:cstate_driver] != 'none'
+  cstate_names = execute('cat /sys/devices/system/cpu/cpu0/cpuidle/state*/name') rescue nil
+  
+  # Attempt to force CPU to enter or leave idle state (it updates /sys/devices/system/cpu/cpu*/cpuidle/state*/)
+
+  # convert to array of int
+  a = execute("cat /sys/devices/system/cpu/cpu*/cpuidle/state*/usage").map!{|x| x.to_i}
+  execute("stress -t 1 -c #{cpu[:nb_threads]}") rescue nil
+  sleep 1
+  b = execute("cat /sys/devices/system/cpu/cpu*/cpuidle/state*/usage").map!{|x| x.to_i}
+  # b-a elements by elements and also group by cpu_id
+  diff = [b, a].transpose.map {|v| v.reduce(:-)}.each_slice(cstate_names.size).to_a
+  # sum states of all the cpus
+  diff = diff.transpose.map{|v| v.reduce(:+)}
+
+  # deeper cstate ?
+  cpu[:cstate_max_id] = diff.size - diff.reverse.index { |x| x != 0 } - 1
+end
+
+# bios configuration (using sysctl)
+
+syscfg_list = { 
+  :bios_ht_enabled => 'LogicalProc',
+  :bios_turboboost_enabled => 'ProcTurboMode',
+  :bios_c1e => 'ProcC1E',
+  :bios_cstates => 'ProcCStates',
+}
+
+syscfg = execute('syscfg') rescue []
+syscfg_list.each {|k,v|
+  cpu[k] = (syscfg.match(/^[;]?#{v}=(.*)/)[1] == 'enable' rescue 'unknown')
+}
+
+#cpu[:extra2] = 'test2'
