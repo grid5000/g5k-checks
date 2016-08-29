62e8eeb898706621fcc7e03f640f93501fddeb6d
Jérémie Gaidamour <jeremie.gaidamour@inria.fr>
2016-08-29 16:56:56 +0200

    [architecture] Removed smt_size and smp_size

--- lib/g5kchecks/spec/architecture/architecture_spec.rb
+++ lib/g5kchecks/spec/architecture/architecture_spec.rb
@@ -5,24 +5,6 @@ describe "Architecture" do
     @system = RSpec.configuration.node.ohai_description
   end
 
-  it "should have the correct number of core" do
-    core_api = 0
-    core_api = @api['smp_size'].to_i if @api
-    if @system[:cpu][:real] == 0
-      core_ohai = @system[:cpu][:total].to_i
-    else
-      core_ohai = @system[:cpu][:real].to_i
-    end
-    core_ohai.should eql(core_api), "#{core_ohai}, #{core_api}, architecture, smp_size"
-  end
-
-  it "should have the correct number of thread" do
-    threads_api = 0
-    threads_api = @api['smt_size'].to_i if @api
-    threads_ohai = @system[:cpu][:total].to_i
-    threads_ohai.to_i.should eql(threads_api), "#{threads_ohai}, #{threads_api}, architecture, smt_size"
-  end
-
   it "should have the correct platform type" do
     plat_api = ""
     plat_api = @api['platform_type'] if @api
