70d1fa24f92e0863d666f08d14165710a4d4cb46
Florent Didier <florent.didier@inria.fr>
2017-01-12 15:58:15 +0100

    Add unstable_device_name property management

--- lib/g5kchecks/spec/disk/disk_spec.rb
+++ lib/g5kchecks/spec/disk/disk_spec.rb
@@ -10,6 +10,23 @@ describe "Disk" do
     end
   end
 
+  def get_api_value(k, v, key)
+    return '' if !(@api && @api[k] && @api[k][key])
+    api = if @api[k]['unstable_device_name']
+            unstable_device_value(key, v)
+          else # Default case : no reordering
+            @api[k][key]
+          end
+    return api
+  end
+
+  # If devices names are unstable : reorder devices taken from the ref-api
+  def unstable_device_value(key, v)
+    return '' if !(v && v['by_id'])
+    api = @api.values.select { |x| x['by_id'] == v['by_id'] }.first
+    return api[key]
+  end
+
   RSpec.configuration.node.ohai_description.block_device.select { |key,value| key =~ /[sh]d.*/ and value["model"] != "vmDisk" }.each { |k,v|
 
     it "should have the correct name" do
@@ -19,30 +36,27 @@ describe "Disk" do
 
     it "should have the correct device id" do
       by_id_ohai = v['by_id']
-      by_id_api = ''
-      by_id_api = @api[k]['by_id'] if (@api and @api[k] and @api[k]['by_id'])
+      by_id_api = get_api_value(k, v, 'by_id')
       by_id_ohai.should eql(by_id_api), "#{by_id_ohai}, #{by_id_api}, storage_devices, #{k}, by_id"
     end
 
     it "should have the correct device path" do
       by_path_ohai = ''
       by_path_ohai = v['by_path'] if v.key?('by_path') && v['by_path'] != nil
-      by_path_api = ''
-      by_path_api = @api[k]['by_path'] if (@api and @api[k] and @api[k]['by_path'])
+      by_path_api = get_api_value(k, v, 'by_path')
       by_path_ohai.should eql(by_path_api), "#{by_path_ohai}, #{by_path_api}, storage_devices, #{k}, by_path"
     end
 
     it "should have the correct size" do
       size_ohai = v["size"].to_i*512
-      size_api = 0
-      size_api = @api[k]["size"].to_i if (@api and @api[k] and @api[k]["size"])
+      size_api = get_api_value(k, v, 'size')
+      size_api = 0 if size_api == ''
       size_ohai.should eql(size_api), "#{size_ohai}, #{size_api}, storage_devices, #{k}, size"
     end
 
     it "should have the correct model" do
       model_ohai = v['model']
-      model_api = ""
-      model_api = @api[k]['model'] if (@api and @api[k] and @api[k]['model'])
+      model_api = get_api_value(k, v, 'model')
       model_ohai.should eql(model_api), "#{model_ohai}, #{model_api}, storage_devices, #{k}, model"
     end
 
@@ -50,23 +64,16 @@ describe "Disk" do
       version_ohai = v["rev"]
       # See github issue #6: ohai gets the 'rev' info from /sys/block/sda/device/rev and the data is actually truncated on some clusters. Use rev_from_hdparm instead when available
       version_ohai = v["rev_from_hdparm"] if v.key?("rev_from_hdparm") && v["rev_from_hdparm"] != nil
-
-      version_api = ""
-      version_api = @api[k]['rev'] if (@api and @api[k] and @api[k]['rev'])
-      version_api = Utils.string_to_object(version_api.to_s)
       version_ohai = Utils.string_to_object(version_ohai.to_s)
+      version_api = get_api_value(k, v, 'rev')
       version_ohai.should eql(version_api), "#{version_ohai}, #{version_api}, storage_devices, #{k}, rev"
     end
 
     it "should have the correct vendor" do
       vendor_ohai = v["vendor"]
       vendor_ohai = v["vendor_from_lshw"] if v.key?("vendor_from_lshw") && v["vendor_from_lshw"] != nil
-
-      vendor_api = ""
-      vendor_api = @api[k]['vendor'] if (@api and @api[k] and @api[k]['rev'])
+      vendor_api = get_api_value(k, v, 'vendor')
       vendor_ohai.should eql(vendor_api), "#{vendor_ohai}, #{vendor_api}, storage_devices, #{k}, vendor"
     end
-
   }
-
 end
