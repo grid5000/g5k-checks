1443eb57fe28edcb99b7b6a5934e57951fa0689b
Nicolas Michon <nicolas.michon@inria.fr>
2017-02-28 13:04:48 +0100

    Removed network interface enabled lookup

--- lib/g5kchecks/ohai/network_adapters.rb
+++ lib/g5kchecks/ohai/network_adapters.rb
@@ -1,5 +1,3 @@
-require 'resolv'
-
 provides "network/network_adapters"
 require_plugin("network")
 require_plugin("hostname")
@@ -12,47 +10,19 @@ interfaces.select { |d,i| %w{ eno eth myri }.include?(i[:type]) }.each do |dev,i
   # accessible from the OS.
   iface[:management] = false
 
-  #  iface[:vendor] = nil #FIXME: NOT IMPLEMENTED
-  #  iface[:version] = nil #FIXME: NOT IMPLEMENTED
-
   # Get MAC address
-  # ruby 1.9
+  # ruby >= 1.9
   iface[:mac] = iface[:addresses].select{|key,value| value == {'family'=>'lladdr'}}.key({'family'=>'lladdr'})
-  # ruby 1.8
-  #iface[:mac] = iface[:addresses].select{|key,value| value == {'family'=>'lladdr'}}[0][0]
 
-  #save the state of interface to restore it afterwards, defaults to up
-  ifaceState = "up"
-  popen4("cat /sys/class/net/#{dev}/operstate") do |pid, stdin, stdout, stderr|
-    stdout.each do |line|
-      case line.chomp()
-      when "unknown", "lowerlayerdown", "testing", "dormant", "up"
-        ifaceState = "up"
-      when "notpresent", "down"
-        ifaceState = "down"
-      else
-        ifaceState = "up"
-      end
-    end
-  end
-  #Set all interfaces up before calling ethtool. (carrier and rate state are not available if interface is down)
-  system("ip link set dev #{dev} up")
-  #Wait a moment before probing the interface
-  sleep 2
   popen4("ethtool #{dev}; ethtool -i #{dev}") do |pid, stdin, stdout, stderr|
     stdin.close
     stdout.each do |line|
-#      if line =~ /^[[:blank:]]*Link detected: /
-#        iface[:enabled] = ( line.chomp.split(": ").last.eql?('yes') )
-#      end
       if line =~ /^[[:blank:]]*Speed: /
         if line =~ /Unknown/
           iface[:rate] = ""
           # enabled => true if there is any cable connected to this interface (eg speed is know by ethtool)
-          iface[:enabled] = false
         else
           iface[:rate] = line.chomp.split(": ").last.gsub(/([GMK])b\/s/){'000000'}
-          iface[:enabled] = true
         end
       end
       if line =~ /^\s*driver: /
@@ -60,11 +30,7 @@ interfaces.select { |d,i| %w{ eno eth myri }.include?(i[:type]) }.each do |dev,i
       end
     end
   end
-  begin
-    res = Resolv.getaddress(hostname + '-' + dev)
-  rescue Exception => e
-    # No such entry in DNS => ignoring...
-  end
+
   ip = iface[:addresses].select{|key,value| value[:family] == 'inet'}.to_a
   iface[:ip] = ip[0][0] if ip.size > 0
   ip6 = iface[:addresses].select{|key,value| value[:family] == 'inet6'}.to_a
@@ -88,8 +54,6 @@ interfaces.select { |d,i| %w{ eno eth myri }.include?(i[:type]) }.each do |dev,i
     end
   end
   iface[:mounted] = ( not iface[:ip].nil? )
-  #Restore previous interface state
-  system("ip link set dev #{dev} #{ifaceState}")
 end
 
 # Process ib interfaces
@@ -111,7 +75,6 @@ interfaces.select { |d,i| %w{ ib }.include?(i[:type]) }.each do |dev,iface|
   end
   iface[:mac] = guid_prefix + File.read(File.join('/sys/class/net', dev, 'address')).chomp.split(':')[5..20].join(':')
 
-  iface[:enabled] = false #FIXME: NOT IMPLEMENTED
   #iface[:driver]  = nil #FIXME: NOT IMPLEMENTED
 
   ca = ""
@@ -143,15 +106,9 @@ interfaces.select { |d,i| %w{ ib }.include?(i[:type]) }.each do |dev,iface|
       end
     end
   end
-  
+
   # The interface is enabled iff state is 'Active' and /sys/class/infiniband/mlx4_0 exists.
   iface[:enabled] = iface[:enabled] && File.exist?('/sys/class/infiniband/mlx4_0')
-
-  begin
-    res = Resolv.getaddress(hostname + '-' + dev)
-  rescue Exception => e
-    # No such entry in DNS => ignoring...
-  end
 
   ip = iface[:addresses].select{|key,value| value[:family] == 'inet'}.to_a
   iface[:ip] = ip[0][0] if ip.size > 0
