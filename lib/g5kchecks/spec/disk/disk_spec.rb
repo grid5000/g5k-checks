ca252f4f728116add29eed4385a98e90f31fa253
Florent Didier <florent.didier@inria.fr>
2017-03-01 19:01:55 +0100

    Check disks from the ref-api disk list instead of only disks enabled on the node

--- lib/g5kchecks/spec/disk/disk_spec.rb
+++ lib/g5kchecks/spec/disk/disk_spec.rb
@@ -1,79 +1,86 @@
-describe "Disk" do
+describe 'Disk' do
 
-  before(:all) do
-    tmpapi = RSpec.configuration.node.api_description["storage_devices"]
-    if tmpapi != nil
-      @api = {}
-      tmpapi.each{ |d|
-        @api[d["device"]] = d
-      }
-    end
-  end
-
-  def get_api_value(k, v, key)
-    return '' if !(@api && @api[k] && @api[k][key])
-    api = if @api[k]['unstable_device_name']
-            unstable_device_value(key, v)
-          else # Default case : no reordering
-            @api[k][key]
-          end
-    return api
+  def get_api_value(api, ohai, device, key)
+    return nil if !(api && api[device] && api[device][key])
+    return api[device][key] if !(api[device]['unstable_device_name'] && ohai[device] && ohai[device]['by_id'])
+    return unstable_device_value(api, key, ohai[device])
   end
 
   # If devices names are unstable : reorder devices taken from the ref-api
-  def unstable_device_value(key, v)
-    return '' if !(v && v['by_id'])
-    api = @api.values.select { |x| x['by_id'] == v['by_id'] }.first
+  def unstable_device_value(api, key, v)
+    api = api.values.select { |x| x['by_id'] == v['by_id'] }.first
     return api[key]
   end
 
-  RSpec.configuration.node.ohai_description.block_device.select { |key,value| key =~ /[sh]d.*/ and value["model"] != "vmDisk" }.each { |k,v|
+  def get_ohai_value(_api, ohai, device, key)
+    return nil if !(ohai && ohai[device] && ohai[device][key])
+    return ohai[device][key]
+  end
 
-    it "should have the correct name" do
-      name_api = @api[k] if @api
+  api = nil
+  tmpapi = RSpec.configuration.node.api_description['storage_devices']
+  if tmpapi != nil
+    api = {}
+    tmpapi.each do |d|
+      api[d['device']] = d
+    end
+  end
+
+  ohai = RSpec.configuration.node.ohai_description.block_device.select { |key, value| key =~ /[sh]d.*/ && value['model'] != 'vmDisk' }
+
+  # If g5k-checks is called with "-m api" option, then api = nil
+  # and we use ohai as a reference. Else we use api as a reference.
+  reference = api.nil? ? ohai : api
+
+  reference.each do |k, _v|
+    # Need to check the API value here, in order to generate the key 'device'
+    # in the yaml and json output files
+    it 'should have the correct name' do
+      name_api = api[k] if api
       name_api.should_not eql(nil), "#{k}, not_exist, storage_devices, #{k}, device"
     end
 
-    it "should have the correct device id" do
-      by_id_ohai = v['by_id']
-      by_id_api = get_api_value(k, v, 'by_id')
-      by_id_ohai.should eql(by_id_api), "#{by_id_ohai}, #{by_id_api}, storage_devices, #{k}, by_id"
+    it 'should have the correct device id' do
+      by_id_api = get_api_value(api, ohai, k, 'by_id')
+      by_id_ohai = get_ohai_value(api, ohai, k, 'by_id')
+      by_id_api.should eql(by_id_ohai), "#{by_id_ohai}, #{by_id_api}, storage_devices, #{k}, by_id"
     end
 
-    it "should have the correct device path" do
-      by_path_ohai = ''
-      by_path_ohai = v['by_path'] if v.key?('by_path') && v['by_path'] != nil
-      by_path_api = get_api_value(k, v, 'by_path')
-      by_path_ohai.should eql(by_path_api), "#{by_path_ohai}, #{by_path_api}, storage_devices, #{k}, by_path"
+    it 'should have the correct device path' do
+      by_path_api = get_api_value(api, ohai, k, 'by_path')
+      by_path_ohai = get_ohai_value(api, ohai, k, 'by_path')
+      by_path_api.should eql(by_path_ohai), "#{by_path_ohai}, #{by_path_api}, storage_devices, #{k}, by_path"
     end
 
-    it "should have the correct size" do
-      size_ohai = v["size"].to_i*512
-      size_api = get_api_value(k, v, 'size')
-      size_api = 0 if size_api == ''
-      size_ohai.should eql(size_api), "#{size_ohai}, #{size_api}, storage_devices, #{k}, size"
+    it 'should have the correct size' do
+      size_api = get_api_value(api, ohai, k, 'size')
+      size_ohai = 0
+      size = get_ohai_value(api, ohai, k, 'size')
+      size_ohai = size.to_i * 512 if !size.nil?
+      size_api.should eql(size_ohai), "#{size_ohai}, #{size_api}, storage_devices, #{k}, size"
     end
 
-    it "should have the correct model" do
-      model_ohai = v['model']
-      model_api = get_api_value(k, v, 'model')
-      model_ohai.should eql(model_api), "#{model_ohai}, #{model_api}, storage_devices, #{k}, model"
+    it 'should have the correct model' do
+      model_api = get_api_value(api, ohai, k, 'model')
+      model_ohai = get_ohai_value(api, ohai, k, 'model')
+      model_api.should eql(model_ohai), "#{model_ohai}, #{model_api}, storage_devices, #{k}, model"
     end
 
-    it "should have the correct revision" do
-      version_ohai = v["rev"]
-      # See github issue #6: ohai gets the 'rev' info from /sys/block/sda/device/rev and the data is actually truncated on some clusters. Use rev_from_hdparm instead when available
-      version_ohai = v["rev_from_hdparm"] if v.key?("rev_from_hdparm") && v["rev_from_hdparm"] != nil
-      version_ohai = Utils.string_to_object(version_ohai.to_s)
-      version_api = get_api_value(k, v, 'rev')
-      version_ohai.should eql(version_api), "#{version_ohai}, #{version_api}, storage_devices, #{k}, rev"
+    it 'should have the correct revision' do
+      version_api = get_api_value(api, ohai, k, 'rev')
+      # See github issue #6: api gets the 'rev' info from /sys/block/sda/device/rev and the data is actually truncated on some clusters. Use rev_from_hdparm instead when available
+      version_ohai = get_ohai_value(api, ohai, k, 'rev')
+      rev_from_hdparm = get_ohai_value(api, ohai, k, 'rev_from_hdparm')
+      version_ohai = rev_from_hdparm if !rev_from_hdparm.nil?
+      version_api.should eql(version_ohai), "#{version_ohai}, #{version_api}, storage_devices, #{k}, rev"
     end
 
-    it "should have the correct vendor" do
-      vendor_ohai = v["vendor"]
-      vendor_ohai = v["vendor_from_lshw"] if v.key?("vendor_from_lshw") && v["vendor_from_lshw"] != nil
-      vendor_api = get_api_value(k, v, 'vendor')
-      vendor_ohai.should eql(vendor_api), "#{vendor_ohai}, #{vendor_api}, storage_devices, #{k}, vendor"
+    it 'should have the correct vendor' do
+      vendor_api = get_api_value(api, ohai, k, 'vendor')
+      vendor_ohai = get_ohai_value(api, ohai, k, 'vendor')
+      vendor_from_lshw = get_ohai_value(api, ohai, k, 'vendor_from_lshw')
+      vendor_ohai = vendor_from_lshw  if !vendor_from_lshw.nil?
+      vendor_api.should eql(vendor_ohai), "#{vendor_ohai}, #{vendor_api}, storage_devices, #{k}, vendor"
     end
-  }
+  end
 end
