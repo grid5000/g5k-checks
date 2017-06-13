9fba1d63f2c0d501f9434ce2e5645c4042ca7354
Nicolas Michon <nicolas.michon@inria.fr>
2017-06-13 18:03:28 +0200

    Fixed bmc specs

--- lib/g5kchecks/spec/bmc/bmc_spec.rb
+++ lib/g5kchecks/spec/bmc/bmc_spec.rb
@@ -4,31 +4,31 @@ describe "BMC" do
     @api = RSpec.configuration.node.api_description["network_adapters"].select { |na|
       na['management'] == true
     }[0] unless RSpec.configuration.node.api_description.empty?
-    @ohai = RSpec.configuration.node.ohai_description[:network][:interfaces][:bmc].to_hash rescue nil
+    @ohai = RSpec.configuration.node.ohai_description[:network][:interfaces][:bmc] rescue nil
   end
 
-    it "should have the correct IPv4" do
-      ip_api = ""
-      ip_ohai = ""
-      ip_api = @api['ip'] if @api
-      ip_ohai = @ohai['ip'] if @ohai 
-      expect(ip_ohai).to eql(ip_api), "#{ip_ohai}, #{ip_api}, network_adapters, bmc, ip"
-    end
+  it "should have the correct IPv4" do
+    ip_api = ""
+    ip_ohai = ""
+    ip_api = @api['ip'] if @api
+    ip_ohai = @ohai['ip'] if @ohai 
+    expect(ip_ohai).to eql(ip_api), "#{ip_ohai}, #{ip_api}, network_adapters, bmc, ip"
+  end
 
-    it "should have the correct Mac Address" do
-      mac_api = ""
-      mac_ohai = ""
-      mac_api = @api['mac'] if @api
-      mac_ohai = @ohai['mac'] if @ohai
-      expect(mac_ohai).to eql(mac_api), "#{mac_ohai}, #{mac_api}, network_adapters, bmc, mac"
-    end
+  it "should have the correct Mac Address" do
+    mac_api = ""
+    mac_ohai = ""
+    mac_api = @api['mac'] if @api
+    mac_ohai = @ohai['mac'] if @ohai
+    expect(mac_ohai).to eql(mac_api), "#{mac_ohai}, #{mac_api}, network_adapters, bmc, mac"
+  end
 
-    it "should be a management card" do
-      mgt_api = nil
-      mgt_ohai = nil
-      mgt_api = @api['management'] if @api
-      mgt_ohai = @ohai['management'] if @ohai
-      expect(mgt_ohai).to eql(mgt_api), "#{mgt_ohai}, #{mgt_api}, network_adapters, bmc, management"
-    end
+  it "should be a management card" do
+    mgt_api = nil
+    mgt_ohai = nil
+    mgt_api = @api['management'] if @api
+    mgt_ohai = @ohai['management'] if @ohai
+    expect(mgt_ohai).to eql(mgt_api), "#{mgt_ohai}, #{mgt_api}, network_adapters, bmc, management"
+  end
 end
 
