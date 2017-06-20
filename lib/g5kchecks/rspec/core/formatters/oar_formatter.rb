76b21cfafaf7b52476818be113537df60a020cd1
Nicolas Michon <nicolas.michon@inria.fr>
2017-06-20 17:57:49 +0200

    No more special cases in oar formatter: every failed test result in checklog file creation

--- lib/g5kchecks/rspec/core/formatters/oar_formatter.rb
+++ lib/g5kchecks/rspec/core/formatters/oar_formatter.rb
@@ -11,18 +11,7 @@ module RSpec
         end
 
         def example_failed(failedExampleNotification)
-          array = failedExampleNotification.exception.message.split(', ')
-          # pas super beau, pour distinguer plusieurs composants
-          # typiquement pour disk et network
-          if array[0] =~ /mount/
-            file_name = array[0]
-	  elsif array[-2] =~ /\d{1,2}/
-            file_name = failedExampleNotification.example.full_description.gsub(" ","_") + "_" + array[-2]
-          elsif array[-2] =~ /sd./
-            file_name = failedExampleNotification.example.full_description.gsub(" ","_") + "_" + array[-2]
-	  else
-            file_name = failedExampleNotification.example.full_description.gsub(" ","_")
-          end
+          file_name = failedExampleNotification.example.description.strip
 	  file_name = file_name.gsub(/\//,'\\').gsub(" ","_")
           File.open(File.join(RSpec.configuration.output_dir, "OAR_"+file_name), 'w') do |f|
             f.puts failedExampleNotification.example.execution_result.exception.to_json
