cbff9e6bd208e50a70d13cb844a331634d1035b5
Nicolas Michon <nicolas.michon@inria.fr>
2017-06-26 11:36:54 +0200

    Ignore skip disk checks in API mode

--- lib/g5kchecks/spec/disk/disk_spec.rb
+++ lib/g5kchecks/spec/disk/disk_spec.rb
@@ -26,7 +26,10 @@ describe 'Disk' do
     end
   end
 
-  if RSpec.configuration.node.ohai_description["g5k"]["kadeploy"]["user_deployed"] == true
+  #Skipping disk checks if we are in a user deployed env
+  #Ignore if api mode to dump correct information
+  #TODO improve this once we have correct calls in AOR rest API
+  if RSpec.configuration.node.ohai_description["g5k"]["kadeploy"]["user_deployed"] == true && RSpec.configuration.node.conf["mode"] != "api"
     puts "Skipping disk tests on user-deployed environment with reservable disks"
     break
   end
