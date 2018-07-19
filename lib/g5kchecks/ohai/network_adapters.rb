55d96b09a3bb98ab8b238c07a9ff3a2ace22636c
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-07-19 10:50:14 +0200

    network_adapters: remove incorrect comment

--- lib/g5kchecks/ohai/network_adapters.rb
+++ lib/g5kchecks/ohai/network_adapters.rb
@@ -27,7 +27,6 @@ Ohai.plugin(:NetworkAdapters) do
     }
 
     # Process all but bridge, infiniband and loopback
-    # Paralell loop
     interfaces.reject{ |d,i| %w{ ib br }.include?(i[:type]) || d == "lo" }.each do |dev, iface|
 
       # Likely not a management interface if it is accessible from the OS.
