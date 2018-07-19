49e773ede316876103887d7af850b074be8e4a96
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-07-19 10:37:38 +0200

    testdisk: only run tests if there's a value in API

--- lib/g5kchecks/spec/testdisk/testdisk_spec.rb
+++ lib/g5kchecks/spec/testdisk/testdisk_spec.rb
@@ -15,35 +15,43 @@ describe "TestDisk" do
 
   RSpec.configuration.node.ohai_description["block_device"].select { |key,value| key =~ /[sh]da/ and value["model"] != "vmDisk" }.each { |k,v|
 
-    it "should give good results in read" do
-      test_file = File.expand_path("../../data/read.fio", File.dirname(__FILE__))
-      stdout = Utils.shell_out("fio #{test_file}").stdout
-      stdout.each_line do |line|
-        if line =~ /READ/
-          maxt_read = Hash[line.split(',').collect!{|s| s.strip.split('=')}]['maxt'].scan(/\d+/)[0].to_i
-          maxt_read_api = 0
-          maxt_read_api = @api[k]["timeread"].to_i if (@api and @api[k] and @api[k]["timeread"])
-	  err = (maxt_read-maxt_read_api).abs
-	  expected =  maxt_read_api/10
-          Utils.test(err, expected, "storage_devices/#{k}/timeread", true) do |v_system, v_api, error_msg|
-            expect(v_system).to be < v_api, error_msg
+    maxt_read_api = 0
+    maxt_read_api = @api[k]["timeread"].to_i if (@api and @api[k] and @api[k]["timeread"])
+    if maxt_read_api != 0
+
+      it "should give good results in read" do
+        test_file = File.expand_path("../../data/read.fio", File.dirname(__FILE__))
+        stdout = Utils.shell_out("fio #{test_file}").stdout
+        stdout.each_line do |line|
+          if line =~ /READ/
+            maxt_read = Hash[line.split(',').collect!{|s| s.strip.split('=')}]['maxt'].scan(/\d+/)[0].to_i
+            err = (maxt_read-maxt_read_api).abs
+            expected =  maxt_read_api/10
+            Utils.test(err, expected, "storage_devices/#{k}/timeread", true) do |v_system, v_api, error_msg|
+              expect(v_system).to be < v_api, error_msg
+            end
           end
         end
       end
+
     end
   
-    it "should give good results in write" do
-      test_file = File.expand_path("../../data/write.fio", File.dirname(__FILE__))
-      stdout = Utils.shell_out("fio #{test_file}").stdout
-      stdout.each_line do |line|
-        if line =~ /WRITE/
-          maxt_write = Hash[line.split(',').collect!{|s| s.strip.split('=')}]['maxt'].scan(/\d+/)[0].to_i
-          maxt_write_api = 0
-          maxt_write_api = @api[k]["timewrite"].to_i if (@api and @api[k] and @api[k]["timewrite"])
-       	  err = (maxt_write-maxt_write_api).abs
-	  expected =  maxt_write_api/10
-          Utils.test(err, expected, "storage_devices/#{k}/timewrite", true) do |v_system, v_api, error_msg|
-            expect(v_system).to be < v_api, error_msg
+    maxt_write_api = 0
+    maxt_write_api = @api[k]["timewrite"].to_i if (@api and @api[k] and @api[k]["timewrite"])
+
+    if maxt_write_api != 0
+
+      it "should give good results in write" do
+        test_file = File.expand_path("../../data/write.fio", File.dirname(__FILE__))
+        stdout = Utils.shell_out("fio #{test_file}").stdout
+        stdout.each_line do |line|
+          if line =~ /WRITE/
+            maxt_write = Hash[line.split(',').collect!{|s| s.strip.split('=')}]['maxt'].scan(/\d+/)[0].to_i
+            err = (maxt_write-maxt_write_api).abs
+            expected =  maxt_write_api/10
+            Utils.test(err, expected, "storage_devices/#{k}/timewrite", true) do |v_system, v_api, error_msg|
+              expect(v_system).to be < v_api, error_msg
+            end
           end
         end
       end
