b3476a8651e66275955f94d836e0ea1af09d96b0
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-07-19 10:41:37 +0200

    clock: no need to restart ntp

--- lib/g5kchecks/spec/clk/clk_spec.rb
+++ lib/g5kchecks/spec/clk/clk_spec.rb
@@ -1,8 +1,6 @@
 describe "Clock" do
 
-  #TODO update this test, ntpdate is deprecated and should be replaced by ntpd --....
-  it "should have the correct hardware clock" do
-    %x{/etc/init.d/ntp stop;/usr/sbin/ntpdate ntp;/etc/init.d/ntp start}
+  it "should be in sync with hardware clock" do
     hwdate = %x{date -d "`/sbin/hwclock --utc`" +"%s"}
     osdate = %x{date -u +"%s"}
     if (hwdate.to_i - osdate.to_i).abs > 1
