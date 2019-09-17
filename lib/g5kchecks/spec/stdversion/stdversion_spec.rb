3f73756c1b960bb2c5ca5b5d21088f8c2f00f75f
fsaintma <frederic.saint-marcel@inria.fr>
2019-09-17 16:22:28 +0200

    skip env version test in case of job deploy

--- lib/g5kchecks/spec/stdversion/stdversion_spec.rb
+++ lib/g5kchecks/spec/stdversion/stdversion_spec.rb
@@ -7,13 +7,19 @@ describe "Standard Environment Version" do
     @g5k = RSpec.configuration.node.ohai_description["g5k"]
   end
 
+  # user_deployed is true = job of type deploy
+  # we don't verify the environment version
   it "should have environment version equals to standard env in kadeploy api" do
-    lastV = @g5k["kadeploy"]["stdenv"]["version"]
-    lastN = @g5k["kadeploy"]["stdenv"]["name"]
-    lastStd = "#{lastN}-#{lastV}"
-    stdNameVersion = @g5k["env"]["name"]
-    Utils.test(stdNameVersion, lastStd, "Standard Environment Version", true) do |v_system, v_api, error_msg|
-      expect(v_system).to eql(v_api), error_msg
+    if @g5k["user_deployed"]
+      skip
+    else
+      lastV = @g5k["kadeploy"]["stdenv"]["version"]
+      lastN = @g5k["kadeploy"]["stdenv"]["name"]
+      lastStd = "#{lastN}-#{lastV}"
+      stdNameVersion = @g5k["env"]["name"]
+      Utils.test(stdNameVersion, lastStd, "Standard Environment Version", true) do |v_system, v_api, error_msg|
+        expect(v_system).to eql(v_api), error_msg
+      end
     end
   end
 
