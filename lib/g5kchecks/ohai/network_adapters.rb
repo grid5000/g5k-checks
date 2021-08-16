06ad96ffb853e773506780f09d02eeaa03a9ca8e
Samir Noir <samir.noir@inria.fr>
2021-08-16 14:35:20 +0200

    [network] optimize ethtool waiting time for down interfaces
    Waiting for interfaces to be up can be long in some cases (like if STP
    is used on the switch). Since we cannot known in advance if an interface
    will eventually be up or down (e.g: no carrier) a retry loop with a
    timeout was used. This was causing delay for clusters where interfaces
    were coming up fast since we had to wait the end of the timeout to act
    that down interfaces were "really down", without being to optimistic and
    miss interfaces that were supposed to be up.
    
    By using the reference api file presents on each node, we can predict
    that an interface is supposed to have no carrier and skip the waiting
    loop. This is not applicated to -m api mode which needs to be certain of
    interfaces status.

--- lib/g5kchecks/ohai/network_adapters.rb
+++ lib/g5kchecks/ohai/network_adapters.rb
@@ -5,6 +5,7 @@ require 'peach'
 
 Ohai.plugin(:NetworkAdapters) do
   provides 'network/network_adapters'
+  depends 'g5k'
   depends 'network'
   depends 'hostname'
   depends 'chassis'
@@ -55,32 +56,35 @@ Ohai.plugin(:NetworkAdapters) do
         # true if predictable names are in use
         iface[:use_predictable_name] = iface[:name] == dev
 
-        now = Time.now.to_i
-        timeout = if Utils.interface_operstate(dev) == 'down'
-                    15
-                  else
-                    # If interface was already up, reduce timeout
-                    5
-                  end
+        api_desc = g5k.dig('local_api_description', 'network_adapters')&.select { |i| i['name'] == dev }&.first
+        ethtool_infos = Utils.interface_ethtool(dev)
 
-        # Wait for link negociation after setting iface up
-        ethtool_infos = {}
-        ethtool = Utils.interface_ethtool(dev)
-        while Time.now.to_i < now + timeout
-          sleep(1)
-          status = Utils.interface_operstate(dev)
-          # Make probing quicker if interface stays down
-          timeout -= 0.5 if status == 'down'
-          # Get infos only if interesting or is last run
-          next unless status != 'down' || Time.now.to_i >= now + timeout
+        if api_desc&.dig('mountable') || api_desc.nil?
+          now = Time.now.to_i
+          timeout = if Utils.interface_operstate(dev) == 'down'
+                      25
+                    else
+                      # If interface was already up, reduce timeout
+                      5
+                    end
 
-          ethtool_infos = Utils.interface_ethtool(dev)
-          # exit early if rate changed
-          if !ethtool_infos[:rate].nil? &&
-              (ethtool_infos[:rate] != ethtool[:rate] || ethtool_infos[:rate].to_i != 0)
-            break
+          # Wait for link negociation after setting iface up
+          ethtool = Utils.interface_ethtool(dev)
+          while Time.now.to_i < now + timeout
+            sleep(0.5)
+            status = Utils.interface_operstate(dev)
+            # Get infos only if interesting or is last run
+            next unless status != 'down' || Time.now.to_i >= now + timeout
+
+            ethtool_infos = Utils.interface_ethtool(dev)
+            # exit early if rate changed
+            if !ethtool_infos[:rate].nil? &&
+                (ethtool_infos[:rate] != ethtool[:rate] || ethtool_infos[:rate].to_i != 0)
+              break
+            end
           end
         end
+
         iface[:rate] = ethtool_infos[:rate]
         iface[:driver] = ethtool_infos[:driver]
         iface[:firmware_version] = ethtool_infos[:firmware_version]
