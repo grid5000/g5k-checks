bab75195fea287fc8bbbdf5da90b6df43d337285
Samir Noir <samir.noir@inria.fr>
2021-10-26 14:27:46 +0200

    [utils/lshw] use nokogiri to parse lshw output to get memory devices

--- lib/g5kchecks/utils/lshw.rb
+++ lib/g5kchecks/utils/lshw.rb
@@ -9,18 +9,18 @@ module LsHw
 
   def self.get_memory_devices
     lshw_xml_output = Utils.shell_out('lshw -xml -C memory').stdout
-    xml_doc = REXML::Document.new(lshw_xml_output)
+    xml_doc = Nokogiri::XML(lshw_xml_output)
     memory_devices = {}
 
-    REXML::XPath.match(xml_doc, "//node[contains(@id,'bank:')]").each do |d|
-      dev_id = d.elements['slot'][0].to_s.gsub(/Physical:.*(DIMM)(\d+)$/, '\1_\2').downcase
+    xml_doc.xpath("//node[contains(@id,'bank:')]").each do |d|
+      dev_id = d.at_xpath("slot").text.gsub(/Physical:.*(DIMM)(\d+)$/, '\1_\2').downcase
       if memory_devices.has_key?(dev_id)
         raise "Overlapping detected, the memory device name #{dev_id} (generated) is already " \
           "present in the data structure"
       end
       memory_devices[dev_id] = {}
-      memory_devices[dev_id][:size] = d.elements['size'][0].to_s.to_i
-      if d.elements['description'][0].to_s.match?(/DDR/)
+      memory_devices[dev_id][:size] = d.at_xpath("size").text.to_i
+      if d.at_xpath('description').text.match?(/DDR/)
         memory_devices[dev_id][:technology] = :dram
       else
         raise 'This kind of memory is not yet supported while using lshw'
