37d9d9e978fb7a6016e3a1845bf32f2ea8a7950e
Simon Delamare <simon.delamare@ens-lyon.fr>
2024-06-07 13:46:12 +0200

    hydra wip

--- lib/g5kchecks/spec/mem/mem_spec.rb
+++ lib/g5kchecks/spec/mem/mem_spec.rb
@@ -14,9 +14,11 @@ describe 'Memory' do
                  Utils.lshw_total_memory(:dram)
                end
 
-    err = ((size_sys - size_api) / size_api.to_f).abs
-    Utils.test(size_sys, size_api, 'main_memory/ram_size') do |_v_ohai, _v_api, error_msg|
-      expect(err).to be < 0.15, error_msg
+    unless size_sys.nil? && (size_api == 0)
+      err = ((size_sys - size_api) / size_api.to_f).abs
+      Utils.test(size_sys, size_api, 'main_memory/ram_size') do |_v_ohai, _v_api, error_msg|
+        expect(err).to be < 0.15, error_msg
+      end
     end
   end
 
@@ -56,35 +58,43 @@ describe 'MemoryDevices' do
   reference_devices = api_devices.nil? ? ohai_devices : api_devices
 
   it 'should have the correct number of devices' do
-    size_ohai = ohai_devices.length
-    size_api = api_devices.nil? ? nil : api_devices.length
-    Utils.test(size_ohai, size_api, "memory_devices/length", true) do |v_ohai, v_api, error_msg|
-      expect(v_ohai).to eql(v_api), error_msg
+    unless ohai_devices.empty? #FIXME: check there is no DIMM in API
+      size_ohai = ohai_devices.length
+      size_api = api_devices.nil? ? 0 : api_devices.length
+      Utils.test(size_ohai, size_api, "memory_devices/length", true) do |v_ohai, v_api, error_msg|
+        expect(v_ohai).to eql(v_api), error_msg
+      end
     end
   end
 
   reference_devices.each_key do |name|
     it 'should have the correct size' do
-      size_ohai = ohai_devices[name][:size]
-      size_api = api_devices.nil? ? nil : api_devices[name][:size]
-      Utils.test(size_ohai, size_api, "memory_devices/#{name}/size") do |v_ohai, v_api, error_msg|
-        expect(v_ohai).to eql(v_api), error_msg
+      unless ohai_devices.empty? #FIXME: check there is no DIMM in API
+        size_ohai = ohai_devices[name][:size]
+        size_api = api_devices.nil? ? nil : api_devices[name][:size]
+        Utils.test(size_ohai, size_api, "memory_devices/#{name}/size") do |v_ohai, v_api, error_msg|
+          expect(v_ohai).to eql(v_api), error_msg
+        end
       end
     end
 
     it 'should have the correct firmware' do
-      firmware_ohai = ohai_devices[name][:firmware]
-      firmware_api = api_devices.nil? ? nil : api_devices[name][:firmware]
-      Utils.test(firmware_ohai, firmware_api, "memory_devices/#{name}/firmware") do |v_ohai, v_api, error_msg|
-        expect(v_ohai).to eql(v_api), error_msg
+      unless ohai_devices.empty? #FIXME: check there is no DIMM in API
+        firmware_ohai = ohai_devices[name][:firmware]
+        firmware_api = api_devices.nil? ? nil : api_devices[name][:firmware]
+        Utils.test(firmware_ohai, firmware_api, "memory_devices/#{name}/firmware") do |v_ohai, v_api, error_msg|
+          expect(v_ohai).to eql(v_api), error_msg
+        end
       end
     end
 
     it 'should have the correct technology' do
-      technology_ohai = ohai_devices[name][:technology].to_s
-      technology_api = api_devices.nil? ? nil : api_devices[name][:technology]
-      Utils.test(technology_ohai, technology_api, "memory_devices/#{name}/technology") do |v_ohai, v_api, error_msg|
-        expect(v_ohai).to eql(v_api), error_msg
+      unless ohai_devices.empty? #FIXME: check there is no DIMM in API
+        technology_ohai = ohai_devices[name][:technology].to_s
+        technology_api = api_devices.nil? ? nil : api_devices[name][:technology]
+        Utils.test(technology_ohai, technology_api, "memory_devices/#{name}/technology") do |v_ohai, v_api, error_msg|
+          expect(v_ohai).to eql(v_api), error_msg
+        end
       end
     end
   end
